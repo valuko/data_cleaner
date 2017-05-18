96c470766d5fa4211b6751b9cde926b4e6073522
Victor Aluko <victor.aluko@gmail.com>
2017-05-18 15:34:39 +0300

    Created data cleaner to clean the data

---
+++ data_cleaner.py
@@ -0,0 +1,61 @@
+import csv
+
+
+def gencolnamehashmap(colnames_file):
+    cleaned = dict()
+    with open(colnames_file, newline='') as csvfile:
+        column_names = csv.reader(csvfile, delimiter=',')
+        for row in column_names:
+            col = row[0].strip(' \t\n\r')
+            cleaned[col] = None
+    return cleaned
+
+
+def read_csv_cols(filename, col_hash):
+    with open(filename, newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        csv_headings = next(csv_reader)
+        i = 0
+        for heading in csv_headings:
+            if heading in col_hash:
+                col_hash[heading] = i
+            i += 1
+    return col_hash
+
+
+def clean_up_data(filename, cleaned_data):
+    response_data = []
+    with open(filename, newline='') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            sanitized = []
+            idx = 0
+            for val in row:
+                if idx in cleaned_data.values():
+                    sanitized.append(val)
+                idx += 1
+            response_data.append(sanitized)
+    return response_data
+
+
+def write_list_to_file(filename, data_list):
+    with open(filename, "w", newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(data_list)
+
+columns_hash = gencolnamehashmap('sample_colname.csv')
+
+#print(', '.join(columns_hash))
+#exit(1)
+
+cleaned_dict = read_csv_cols("dataset_sample.csv", columns_hash)
+
+#for key in cleaned_dict:
+#    print (key, cleaned_dict[key])
+
+refined_dict = clean_up_data("dataset_sample.csv", cleaned_dict)
+
+#for key in refined_dict:
+#    print(key)
+
+write_list_to_file("output_data.csv", refined_dict)
